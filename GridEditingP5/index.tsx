93bc1b3b815e8bf6889da08c540e1972bdbb16e0
Иван Илюшин <iilyushin@samokat.ru>
2024-09-05 01:17:38 +0300

    some fix

--- GridEditingP5/index.tsx
+++ GridEditingP5/index.tsx
@@ -6,16 +6,16 @@ const server = Bun.serve({
         const file = Bun.file(path.replace('/', ''));
 
         if (url.pathname === '/save') {
-            if (req.method === "POST") {
-                const payload = await req.json();
+            if (req.method === 'POST') {
+                const payload = await req.json() as { name: string; data: any[] };
                 await save(payload.name, payload.data)
-                return new Response("Success");
+                return new Response('Success');
             }
         }
 
         if (url.pathname === '/read') {
-            if (req.method === "POST") {
-                const payload = await req.json();
+            if (req.method === 'POST') {
+                const payload = await req.json() as { name: string };
                 const file = await read(payload.name)
                 return new Response(file);
             }
@@ -24,15 +24,15 @@ const server = Bun.serve({
     },
 });
 console.log(`Listening on localhost:${server.port}`);
+console.log(`Open http://localhost:${server.port}/index.html`);
 
 const save = async (path: string, array: any[] = []) => {
-    const file = Bun.file('/Users/ilyusin/Desktop/RoomMapP5/models/' + path);
+    const file = Bun.file('../RoomMapP5/models/' + path);
     return await Bun.write(file, JSON.stringify(array, null, 2));
 }
 
 const read = async (path: string) => {
-    const file = Bun.file('/Users/ilyusin/Desktop/RoomMapP5/models/' + path);
+    const file = Bun.file('../RoomMapP5/models/' + path);
     const pkg = await file.json();
     return JSON.stringify(pkg);
 }
-
